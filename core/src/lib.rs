fd95808440ac2f1e285b6f90531d4be53cc0b502
amanjpro <amanj.sherwany@adgear.com>
2017-10-20 13:35:24 -0400

    Implement helper functions to moving around in a buffer

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -2,6 +2,30 @@ pub mod gap_buffer;
 
 use gap_buffer::GapBuffer;
 
-type LineBuffer = GapBuffer<Vec<char>>;
+type LineBuffer = GapBuffer<char>;
 type Buffer = GapBuffer<LineBuffer>;
 
+
+pub fn seek_to_line(buffer: &mut Buffer, row: usize) {
+    buffer.seek(row);
+}
+
+pub fn seek_to_col(buffer: &mut Buffer, row: usize, col: usize) {
+    buffer.seek(row);
+    if let Some(mut line) = buffer.buffer.get_mut(row) {
+        line.seek(col);
+    }
+}
+
+pub fn delete_line(buffer: &mut Buffer, row: usize) {
+    buffer.seek(row);
+    buffer.delete();
+}
+
+pub fn delete_char(buffer: &mut Buffer, row: usize, col: usize) {
+    buffer.seek(row);
+    if let Some(mut line) = buffer.buffer.get_mut(row) {
+        line.seek(col);
+        line.delete();
+    }
+}
